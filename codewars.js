b61ddc8ec9a155f25bb3e6dd91154f6fd89e72f1
Emanuele Pace <emanuele.10@outlook.it>
2024-09-17 10:00:36 -0400

    codewards file addition"

---
+++ codewars.js
@@ -0,0 +1,14 @@
+// var summation = function (num) {
+// 	// Code here
+// 	for (let i = 0; i <= 2; i++) {
+// 		console.log(i);
+// 	}
+// };
+
+let i = 0;
+
+while (i <= 10) {
+	console.log(i);
+
+	i += 1;
+}
