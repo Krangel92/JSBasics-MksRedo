0afbea5de36ce092c5658fd5bb2b4418b5a88265
Karla Rangel <krangelhdz@gmail.com>
2024-09-20 11:48:34 -0400

    created multiply.js file + ex/notes

---
+++ Functions/multiply.js
@@ -0,0 +1,14 @@
+// Exercise
+// Define a function multiply which takes two arguments and return their product.
+const multiply = (num1, num2) => {
+	return num1 * num2;
+};
+
+multiply(2, 5); // to run code in node REPL & it'd look like this:
+//     > const multiply = (num1, num2) => {
+//     ... return num1 * num2;
+//     ... };
+//     undefined
+//     > multiply(2, 5);
+//     10
+console.log(multiply(2, 5)); //to see output by running file, node multiply.js
