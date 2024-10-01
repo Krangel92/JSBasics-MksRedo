b2b50e16fb7a4971b77f4fe7294a4a0b620a0576
Karla Rangel <krangelhdz@gmail.com>
2024-10-01 11:42:13 -0400

    added exercise result to EqualityOperator.js + created fizzBuzz.js & solved exercise

--- Conditionals/EqualityOperator.js
+++ Conditionals/EqualityOperator.js
@@ -17,9 +17,31 @@
 // - '===' explicitly communicates your intention to perform a strict comparison, making code more readable & undertandable.
 // - Helps maintaining codebase as it reduces potential bugs & unexpected type conversions.
 
-//Example:
+// Example:
 console.log(0 == false); // true (bc of type coercion)
 console.log(0 === false); // false (bc of strict comparison)
 
 console.log("0" == 0); // true (bc of type coercion)
 console.log("0" === 0); // false (bc of strict comparison)
+
+// Exercise:
+// Debug the following code. When calling this function, something doesn't work as expected. What fix can you suggest to solve the problem?
+const isValidLength = (phoneNumber) => {
+	const validLength = 11;
+	if (phoneNumber.length == validLength) {
+		true;
+	} else {
+		false;
+	}
+};
+
+// The result when running it in node should be (if I want to require the function from a file, remember to export it!):
+//   > isValidLength('00');
+//     false
+//   > isValidLength('00112233445');
+//     true
+
+// The above code currently returns 'undefined' when the function is called bc it is NOT returning anything.
+// Also, the strict equality operator is better to avoid bugs due to different data types.
+
+// RESULT after function is fixed:
---
+++ Conditionals/fizzBuzz.js
@@ -0,0 +1,36 @@
+// Exercise:
+// Write a fizzBuzz function in JavaScript. As a reminder, this function should take a
+// number as argument, and then either:
+
+//   - Return "Fizz" if this number is divisible by 3.
+//   - Return "Buzz" if this number is divisible by 5.
+//   - Return "FizzBuzz" if this number is divisible by both 5 and 3.
+//   - Otherwise, Return the number itself.
+
+const fizzBuzz = (num) => {
+	if (num % 3 === 0 && num % 5 === 0) {
+		return "FizzBuzz";
+	} else if (num % 3 === 0) {
+		return "Fizz";
+	} else if (num % 5 === 0) {
+		return "Buzz";
+	} else {
+		return num;
+	}
+};
+
+// to run code in node REPL:
+fizzBuzz(3);
+fizzBuzz(5);
+fizzBuzz(8);
+fizzBuzz(15);
+fizzBuzz(18);
+fizzBuzz(20);
+
+// //to see output by running file, node fizzBuzz.js
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(8));
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(18));
+console.log(fizzBuzz(20));
