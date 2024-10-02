e454c9b1574839ac8d302062ee0d5c4eb9129d8f
Karla Rangel <krangelhdz@gmail.com>
2024-10-02 12:51:08 -0400

    created introLoops.js + notes & example

---
+++ Loops/introLoops.js
@@ -0,0 +1,21 @@
+// Loops in JS are the same as in other languagues.
+
+// Example:
+// To get all numbers from 0 to 10:
+
+// 1. The below works by just using console.log bc i'm not returning a value, I'm simply printing the
+//    value of i to the console when running the file, node introLoops.js
+for (let i = 0; i <= 10; i++) {
+	console.log(i);
+}
+
+// 2. If to the above I changed console.log to 'return' it doesn't work bc the return value within a
+//    loop is NOT inside a function.
+//    So, extra logic needs to be added:
+const loops = () => {
+	let addNumbers = []; // variable of an empty array to hold values/numbers
+	for (let i = 0; i <= 10; i++) {
+		addNumbers.push(i); // adds each value/number of 'i' to the array
+	}
+	return addNumbers; // returns array after finishing the loop. Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+};
