3bc0cf4db01c00e7dd639007f55a6add3f4fda2e
Karla Rangel <krangelhdz@gmail.com>
2024-10-14 09:34:43 +0100

    modifyed Conditionals/fizzBuzz.js to be able to export it

---
+++ Loops/fizzBuzz.js
@@ -0,0 +1,5 @@
+// Exercise
+// This challenge builds on the previous one, where you defined a fizzBuzz function.
+//  1. Write a function fizzbuzzUntil that accepts a number. This function should use a
+//     loop to loop through all numbers from 1 to the given one, call the fizzBuzz function
+//     for the current number and print the result.
