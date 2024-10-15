b774bbfd8c45bad0140d511b972300e3029636a4
Karla Rangel <krangelhdz@gmail.com>
2024-10-15 10:45:38 +0100

    added further notes & exercises to functionsAsValues.js

--- FunctionsAsValues/functionsAsValues.js
+++ FunctionsAsValues/functionsAsValues.js
@@ -16,5 +16,20 @@ console.log(doubleNumber);
 const simpleCalculation = doubleNumber;
 console.log(simpleCalculation);
 
+// If we call the doubleNumber function, newSalary will be the value returned by that function (i.e., 2 * 1000000 so result is 2000000):
 let newSalary = doubleNumber(1000000);
 console.log(newSalary);
+
+// Since functions are VALUES, they can be passed to another function as an ARGUMENT & be used as the RETURN VALUE of a function:
+const uppercaseMessage = (message) => {
+	return message.toUpperCase();
+};
+// The below function accepts as ARGUMENTS a string message & a function. It then calls the given function to do its job:
+const transform = (message, transformFunction) => {
+	return transformFunction(message);
+};
+
+transform("hello", uppercaseMessage);
+console.log(transform("hello", uppercaseMessage)); // to see the result on the console I need to console.log line 32
+//RESULT:
+HELLO;
