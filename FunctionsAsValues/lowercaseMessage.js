943acf9d30888c76c74d61616f48cae1c7c88a2c
Karla Rangel <krangelhdz@gmail.com>
2024-10-17 13:06:21 +0100

    created lowercaseMessage.js

---
+++ FunctionsAsValues/lowercaseMessage.js
@@ -0,0 +1,7 @@
+// Exercise
+//  1.  Declare a function lowercaseMessage that returns the lowercase version of the given string.
+//  2.  Declare a function transform that behaves like the one above:
+//      - takes one string and one function as arguments
+//      - calls the function on the given string.
+//  3. Call transform by giving it the string 'WHY ARE YOU SHOUTING?' and the function lowercaseMessage.
+//     It should return a lowercase version of the message ('why are you shouting?')
