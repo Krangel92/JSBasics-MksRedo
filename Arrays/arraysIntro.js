e917c378b91994c9b2916272627ab719f90b61ec
Karla Rangel <krangelhdz@gmail.com>
2024-10-18 12:43:31 +0100

    created arraysIntro.js + added example exercises

---
+++ Arrays/arraysIntro.js
@@ -0,0 +1,9 @@
+// Example Exercises:
+
+// Assign an array to the constant numbers:
+const numbers = [1, 2, 3, 4];
+
+// Add one element & return a new array:
+const newNumbers = numbers.concat(5);
+
+console.log(newNumbers); // RESULT: [ 1, 2, 3, 4, 5 ]
