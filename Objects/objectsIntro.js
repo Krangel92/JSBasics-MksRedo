509c0c47d4533a36e01b8439f7378956120b3a0f
Karla Rangel <krangelhdz@gmail.com>
2024-10-25 15:52:25 +0100

    added example object & 2 ways of how to access its properties

--- Objects/objectsIntro.js
+++ Objects/objectsIntro.js
@@ -1,2 +1,19 @@
 // In JS, "object" usually refers to a type of key-value structure (like a Python dictionary).
-//
+// Example:
+
+const person = {
+	name: "Maxine",
+	age: 32,
+	address: "London, E1 123",
+};
+
+// 2 ways to access an object's property:
+console.log(person.name); // RESULT: Maxine
+console.log(person["name"]); // RESULT: Maxine
+
+// More for repetition:
+console.log(person.age);
+console.log(person["age"]);
+
+console.log(person.address);
+console.log(person["address"]);
