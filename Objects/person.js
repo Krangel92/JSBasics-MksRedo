d988464b54c30e71ee5036b15accba2b78a14d23
Karla Rangel <krangelhdz@gmail.com>
2024-10-28 16:00:49 +0000

    created person.js + completed exercise

---
+++ Objects/person.js
@@ -0,0 +1,21 @@
+// Exercise
+// An object is composed of key-value pairs. Those values can be any primitive values such as strings or numbers,
+// or they can be more complex things, such as other objects, arrays or event functions.
+//      1. Print the value of the city attribute ('London').
+//      2. Print the value of the second hobbies value ('exercising').
+
+const person = {
+	name: "Karla",
+	age: 32,
+	address: {
+		city: "London",
+		postcode: "E1 123",
+	},
+	hobbies: ["flamenco", "exercising", "hiking"],
+};
+
+// 1.
+console.log(person.address.city);
+
+// 2.
+console.log(person.hobbies[1]);
