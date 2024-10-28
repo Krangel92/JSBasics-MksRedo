81d0cf3caafe85cf10c9e1d526ca4c1acb188c27
Karla Rangel <krangelhdz@gmail.com>
2024-10-28 16:04:14 +0000

    created cohort.js + added challenge instructions

---
+++ Objects/cohort.js
@@ -0,0 +1,7 @@
+// Challenge
+// 1. Declare an object cohort that has the following properties:
+//      - a string name (e.g 'May2022')
+//      - a number id (e.g 1234)
+//      - an array of student names
+// 2. Declare a function that accepts that object as argument and print a message with the following structure:
+//      <COHORT_ID> - <COHORT_NAME> - <NUMBER_OF_STUDENTS> students in this cohort
