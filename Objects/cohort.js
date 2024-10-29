4b66c771459f5abab67a9231d1643665c45a0ea6
Karla Rangel <krangelhdz@gmail.com>
2024-10-29 16:12:00 +0000

    completed the challenge + note

--- Objects/cohort.js
+++ Objects/cohort.js
@@ -5,3 +5,22 @@
 //      - an array of student names
 // 2. Declare a function that accepts that object as argument and print a message with the following structure:
 //      <COHORT_ID> - <COHORT_NAME> - <NUMBER_OF_STUDENTS> students in this cohort
+
+// 1.
+const cohort = {
+	name: "May2024",
+	id: 1234,
+	students: ["Karla", "Matt", "George", "Tara"],
+};
+
+// 2.
+const cohortFunction = (object) => {
+	return `${object.id} - ${object.name} - ${object.students.length} students in this cohort`;
+};
+
+// Pass the cohort object to the function:
+console.log(cohortFunction(cohort));
+
+// NOTE:
+// The cohortFunction takes an argument object and accesses its properties.
+// The cohort object is created outside the function and passed as an argument when calling objectFunction.
