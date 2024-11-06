7e6d7113cf564f575ffc5393b6e42c0f86c8bcdd
Karla Rangel <krangelhdz@gmail.com>
2024-11-06 12:18:46 +0000

    added example using the map method + notes

--- ArrayOperations/filter&mapArrays.js
+++ ArrayOperations/filter&mapArrays.js
@@ -10,7 +10,7 @@ const names = ["Anna", "Li", "Charlie", "Mary", "Jo"];
 
 // 1. Using a predicate function & 'filter' to get only names shorter than 3 letters:
 const isShorterThanThree = (name) => {
-	console.log("line 13... " + name);
+	// console.log("line 13... " + name);
 	if (name.length < 3) {
 		return true;
 	} else {
@@ -21,3 +21,14 @@ const isShorterThanThree = (name) => {
 const namesShorterThanThreeLetters = names.filter(isShorterThanThree);
 
 console.log(namesShorterThanThreeLetters); // RESULT: [ 'Li', 'Jo' ]
+
+// 2. Create a new array of all names uppercased by applying the 'getUppercased' function to all elements in the 'names' array:
+const getUppercased = (name) => {
+	// console.log("line 27... " + name);
+	return name.toUpperCase();
+	// console.log("line 29 ..." + name.toUpperCase());
+};
+
+const uppercasedNames = names.map(getUppercased);
+
+console.log(uppercasedNames); // RESULT: [ 'ANNA', 'LI', 'CHARLIE', 'MARY', 'JO' ]
