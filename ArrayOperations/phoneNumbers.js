03d042c3f84ae73ad7ad03c7caa8ffba188c2a69
Karla Rangel <krangelhdz@gmail.com>
2024-11-07 09:54:09 +0000

    solved the exercise + added notes

--- ArrayOperations/phoneNumbers.js
+++ ArrayOperations/phoneNumbers.js
@@ -1,7 +1,6 @@
 // Exercise
-// 1. Declare a function checkLength that takes a phone number (as a string) as argument, and returns true if this phone number contains 10 characters or less.
-// 2. Now declare a function filterLongNumbers that takes an array of phone numbers. This function should return only numbers that contain 10 characters or less.
-//    It should make use of the function checkLength written previously.
+// Part 1. Declare a function checkLength that takes a phone number (as a string) as argument,
+//         and returns true if this phone number contains 10 characters or less.
 
 const numbers = [
 	"1763687364",
@@ -10,25 +9,26 @@ const numbers = [
 	"aaaaaaaabbbbbbbcccccdddddddd", // this element should be filtered
 ];
 
-// 1.
+// 1. // I'll pass a single phone number as a string
 const checkLength = (phoneNumber) => {
-	// console.log(phoneNumber);
-	if (phoneNumber.length < 10) {
+	//console.log(phoneNumber);
+	if (phoneNumber.length <= 10) {
 		return true;
 	} else {
 		return false;
 	}
 };
 
-checkLength(numbers);
+// Part 2. Now declare a function filterLongNumbers that takes an array of phone numbers.
+//         This function should return only numbers that contain 10 characters or less.
+//         It should make use of the function checkLength written previously.
 
-// 2.
-// const filterLongNumbers = (phoneNumbers) => {
-// 	return phoneNumbers.filter(checkLength);
-// };
+const filterLongNumbers = (phoneNumbersArr) => {
+	return phoneNumbersArr.filter(checkLength); // filter() here takes the array (passed as argm), applies the function (i.e., checkLength) to each element & keeps those elem that pass
+}; // 											   the condition of the function. Once it is done checking all the elem in the array, it then returns a new array ONLY with the elem that passed.
 
-// console.log(filterLongNumbers(numbers));
+console.log(filterLongNumbers(numbers)); // RESULT: [ '1763687364', '4763687363', '7867867862' ]
 
-const filterLongNumbers = numbers.filter(checkLength);
-
-console.log(filterLongNumbers);
+// NOTE:
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+// the filter() method does not change the original array.
