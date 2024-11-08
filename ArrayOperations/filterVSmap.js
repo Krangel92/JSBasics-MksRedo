fb028378381c4db984fcf4f965c0e22962b28d2e
Karla Rangel <krangelhdz@gmail.com>
2024-11-08 11:57:19 +0000

    added notes + ex re map()

--- ArrayOperations/filterVSmap.js
+++ ArrayOperations/filterVSmap.js
@@ -10,3 +10,19 @@ const numbers = [1, 2, 3, 4, 5];
 
 const evenNumbers = numbers.filter((num) => num % 2 === 0);
 console.log(evenNumbers); // [ 2, 4 ]
+
+// map()
+// Purpose: used to create a new array by transforming each item in the original array.
+// How it works: you give map() a function and it runs that function on each item in the array. The
+//               result of eah function call is added to the new array.
+// Ex:
+// You have a list of numbers and want to double each one.
+const numbers2 = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers2.map((num) => num * 2);
+console.log(doubleNumbers); // [ 2, 4, 6, 8, 10 ]
+
+// Summary:
+
+// Use filter() when you want to keep only certain items.
+// Use map() when you want to change each item in some way.
