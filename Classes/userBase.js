7ea3441da12d40480e67bda89881898093b71aac
Karla Rangel <krangelhdz@gmail.com>
2024-11-13 14:58:04 +0000

    used the class in the node REPL + added notes

--- Classes/userBase.js
+++ Classes/userBase.js
@@ -1,7 +1,7 @@
 // Exercise
 
 // Given the following array of User instances (the class from the previous user.js exercise):
-// const users = [new User("Uma"), new User("Josh"), new User("Ollie")];
+// const usersArr = [new User("Uma"), new User("Josh"), new User("Ollie")];
 
 // We'd now like to have a class UserBase that behaves like this:
 //      > const userBase = new UserBase(users);
@@ -42,3 +42,38 @@ class UserBase {
 }
 
 module.exports = UserBase;
+
+// In node REPL:
+
+// 		> const userClass = require('./user2.js');							// TO REQUIRE THE User CLASS
+// 		 undefined
+// 		> const userBaseClass = require('./userBase.js');					// TO REQUIRE THE UserBase CLASS
+// 		 undefined
+// 		> userClass															// TO SEE THIS IS THE User CLASS
+//  	 [class User]
+// 		> userBaseClass														// TO SEE THIS IS THE UserBase CLASS
+// 		 [class UserBase]
+
+// 		> const usersArr1 = [new userClass("Uma"), new userClass("Josh"), new userClass("Ollie")];     // TO CREATE A VARIABLE (AN ARRAY OF NEW USERS)
+// 		 undefined
+// 		> usersArr1
+// 		 [ User { name: 'Uma' }, User { name: 'Josh' }, User { name: 'Ollie' } ]					 // TO SEE THE CONTENT OF THE VARIABLE CREATED
+
+// 		> const userBase2 = new userBaseClass(usersArr1);					 // TO CREATE A NEW INSTACE OF THE CLASS, SO A NEW OBJECT!
+// 		 undefined
+// 		> userBase2															// TO SEE THE NEW OBJECT CREATED
+// 		 UserBase {
+//   		users: [
+//     			User { name: 'Uma' },
+//     			User { name: 'Josh' },
+//     			User { name: 'Ollie' }
+//   			]
+// 			}
+// 		> userBase2.count()													// TO INVOKE THE count() ON THE NEW OBJECT CREATED
+// 		 3
+// 		> userBase2.getNames()												// TO INVOKE THE getNames() ON THE NEW OBJECT CREATED
+// 		 [ 'Uma', 'Josh', 'Ollie' ]
+// 		> userBase2.getIntroductions()										// TO INVOKE THE getIntroductions() ON THE NEW OBJECT CREATED
+// 		 [ 'Hi, my name is Uma', 'Hi, my name is Josh', 'Hi, my name is Ollie' ]
+// 		> userBase2.users[name[1]]										   // HOW DO I ACCESS EACH USER NAME IN THE OBJECT  ????
+// 		 Uncaught ReferenceError: name is not defined
