e3b422aa53fbc6dd1245f7b2dfd874381539fb66
Karla Rangel <krangelhdz@gmail.com>
2024-11-19 10:01:27 +0000

    notes updates

--- Classes/userBase.js
+++ Classes/userBase.js
@@ -1,9 +1,9 @@
 // TO USE THE User CLASS HERE WE NEED TO 'import' THE User CLASS:
 import { User } from "./user2.js";
 
-// Exercise
+// Exercise (instructions)
 // Given the following array of User instances (the class from the previous user.js exercise):
-const usersArr = [new User("Uma"), new User("Josh"), new User("Ollie")];
+// const usersArr = [new User("Uma"), new User("Josh"), new User("Ollie")];
 
 // We'd now like to have a class UserBase that behaves like this:
 //      > const userBase = new UserBase(users);
@@ -26,6 +26,7 @@ const usersArr = [new User("Uma"), new User("Josh"), new User("Ollie")];
 //  - call methods from the User class.
 // Require the two classes into the node REPL to verify it's working.
 
+const usersArr = [new User("Uma"), new User("Josh"), new User("Ollie")];
 class UserBase {
 	constructor(usersArr) {
 		this.users = usersArr;
@@ -46,14 +47,14 @@ class UserBase {
 // Creating a new instance of the class & invocking it's methods by running the file:
 const userBase1 = new UserBase(usersArr);
 
-// console.log(userBase1); 			// UserBase {  users:  [  User { name: 'Uma' }, User { name: 'Josh' }, User { name: 'Ollie' }  ]  }
-// console.log(userBase1.count());			 			// 3
-// console.log(userBase1.getNames()); 					// [ 'Uma', 'Josh', 'Ollie' ]
-// console.log(userBase1.getIntroductions()); 			// [ 'Hi, my name is Uma', 'Hi, my name is Josh', 'Hi, my name is Ollie' ]
-// console.log(userBase1.users[0]); 					// User { name: 'Uma' }
-// console.log(userBase1.users[1]); 					// User { name: 'Josh' }
-// console.log(userBase1.users[2]); 					// User { name: 'Ollie' }
-// console.log(userBase1.getIntroductions()[0]); 		// Hi, my name is Uma
+console.log(userBase1); // UserBase {  users:  [  User { name: 'Uma' }, User { name: 'Josh' }, User { name: 'Ollie' }  ]  }
+console.log(userBase1.count()); // 3
+console.log(userBase1.getNames()); // [ 'Uma', 'Josh', 'Ollie' ]
+console.log(userBase1.getIntroductions()); // [ 'Hi, my name is Uma', 'Hi, my name is Josh', 'Hi, my name is Ollie' ]
+console.log(userBase1.users[0]); // User { name: 'Uma' }
+console.log(userBase1.users[1]); // User { name: 'Josh' }
+console.log(userBase1.users[2]); // User { name: 'Ollie' }
+console.log(userBase1.getIntroductions()[0]); // Hi, my name is Uma
 
 //EXPORTING THE CLASS:
 // module.exports = UserBase;
